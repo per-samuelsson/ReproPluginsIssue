66a0c2df6cc633debf867abcf6efcf9291680f0f
Per Samuelsson <per@starcounter.com>
2018-11-01 10:12:13 +0100

    Repro projects for https://github.com/natemcmaster/DotNetCorePlugins/issues/18

---
+++ src/App1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Lib1;
+using McMaster.NETCore.Plugins;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Lib1.Class1();
+            new Lib2.Class1();
+
+            PluginLoader.CreateFromAssemblyFile(typeof(Program).Assembly.Location);
+        }
+    }
+}
